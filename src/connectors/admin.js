48969a3dad35532a3f5c8729051926446a122091
Ahmed Qudaih <54820415+AhmedQudaih@users.noreply.github.com>
2022-06-14 09:16:01 +0200

    Update admin.js

--- src/connectors/admin.js
+++ src/connectors/admin.js
@@ -9,18 +9,18 @@ import {
 export default function (fetch, apiUrl ){
 
   const generateAdditionalHeaders = (params) => {
-    return { Authorization: 'Bearer mybearertoken' }
+    return { Authorization: 'Bearer '+ localStorage.getItem("accessToken") }
   }
 
   const generateAdminRoute = (params) => {
-    return `/v1/admins/${params.userId? params.userId : ''}`
+    return `/v1/admins/${params.id? params.id : ''}`
   }
   const getAdmin = createGetConnector(fetch, apiUrl, generateAdminRoute, generateAdditionalHeaders)
   const del = createDeleteConnector(fetch, apiUrl, generateAdminRoute, generateAdditionalHeaders)
 
   const getToken = createGetConnector(fetch, apiUrl, generateTokenRoute, generateAdditionalHeaders)
   const generateTokenRoute = (params) => {
-    return `/v1/admins/${params.userId}/access-token`
+    return `/v1/admins/${params.id}/access-token`
   }
 
   const generateConfigRoute = () => {
@@ -29,12 +29,12 @@ export default function (fetch, apiUrl ){
   const getConfig = createGetConnector(fetch, apiUrl, generateConfigRoute)
 
   const generatePatchNameRoute = (params) => {
-    return `/v1/admins/${params.userId}/name`
+    return `/v1/admins/${params.id}/name`
   }
   const updateName = createPatchConnector(fetch, apiUrl, generatePatchNameRoute, generateAdditionalHeaders)
 
   const generatePatchPasswordRoute = (params) => {
-    return `/v1/admins/${params.userId}/password`
+    return `/v1/admins/${params.id}/password`
   }
   const updatePassword = createPatchConnector(fetch, apiUrl, generatePatchPasswordRoute, generateAdditionalHeaders)
 
@@ -70,32 +70,35 @@ export default function (fetch, apiUrl ){
   }
 
   const readOne = function (id){
-    const res = await getAdmin({ userId: id })
+    const res = await getAdmin({ id: id })
     return res;
   }
 
 
   const getAccessToken = function (id){
-    const res = await getToken({ userId: id })
+    const res = await getToken({ id: id })
     return res;
   }
 
   const deletedOne = function(id){
-    const res = await del({ userId: id })
+    const res = await del({ id: id })
     return res
   }
 
   const patchName = function(id, name){
-    const res = await updateName({ userId: id }, {name: name})
+    const res = await updateName({ id: id }, {name: name})
     return res
   }
   const patchPassword = function(id, password){
-    const res = await patchPassword({ userId: id }, {password: password})
+    const res = await patchPassword({ id: id }, {password: password})
     return res
   }
 
   const login = function(formData){
     const res = await postLogin({},{ email:formData.email, password: formData.password})
+    if(res.loginToken){
+      localStorage.setItem("accessToken", res.loginToken);
+    }
     return res
   }
 
@@ -126,8 +129,8 @@ export default function (fetch, apiUrl ){
 
   return {
     admins: { list, readOne, deleteOne, patchName, patchPassword, getAccessToken, login },
-    forgotPassword: { sendForgotPassword, reset },
-    invitation: { sendInvitation, accept },
+    forgotPassword: { send: sendForgotPassword, reset },
+    invitation: { send: sendInvitation, accept },
     config: { getConfig }
   }
 }
