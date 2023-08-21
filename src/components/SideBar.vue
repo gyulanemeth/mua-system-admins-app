5848ae77b067ce60447537affd7f067b1d078d84
Ahmed Qudaih <54820415+AhmedQudaih@users.noreply.github.com>
2023-08-21 06:58:32 +0200

    Update SideBar.vue

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -9,8 +9,8 @@ const route = useRoute()
 
 <v-card class="elevation-4">
 
-    <v-navigation-drawer color="grey-lighten-2" rail rail-width="60" permanent>
-        <v-list bg-color="grey-lighten-2" density="compact" nav>
+    <v-navigation-drawer color="grey-lighten-2" class="rounded-te-lg" rail rail-width="60" permanent>
+        <v-list bg-color="grey-lighten-2" class="rounded-te-lg" density="compact" nav>
             <v-list-item class="justify-center align-center" active active-class=" elevation-4 text-white bg-white">
                 <v-list-item-icon class="text-black">
                     mdi-shield-account-variant-outline
